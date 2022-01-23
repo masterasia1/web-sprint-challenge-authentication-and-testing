c5feeebecb2997638bea891b1f5425604beb4f42
masterasia1 <a.mayfield18@gmail.com>
2022-01-23 10:01:06 -0800

    am1

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,23 @@
+
+
+
+const jwt = require("jsonwebtoken")
+const {JWT_SECRET} = require("../../config/index.js")
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization
+  if(!token){
+    res.status(401).json("token required")
+  }else{
+    jwt.verify(token,JWT_SECRET,(err,decoded)=>{
+      if(err){
+        res.status(401).json("token invalid: " + err.message)
+      }else{
+        req.decodedToken = decoded
+        next()
+      }
+    })
+  }
   /*
     IMPLEMENT
 
@@ -12,3 +30,6 @@ module.exports = (req, res, next) => {
       the response body should include a string exactly as follows: "token invalid".
   */
 };
+
+
+  
