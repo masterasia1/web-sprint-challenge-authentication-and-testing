cfd7ab5f4caa866dbcab49ea1887c148a4d41190
masterasia1 <a.mayfield18@gmail.com>
2022-01-23 11:30:13 -0800

    am2

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,24 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+const request = require("supertest")
+const db = require("../data/dbConfig")
+const server = require("./server.js")
+
+const user = {username: "sty", password: "news"}
+
+beforeAll(async ()=>{
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+beforeEach(async ()=>{
+  await db("users").truncate()
+})
+afterAll(async ()=>{
+  await db.destroy()
+})
+
+it("return number of users", async ()=>{
+  let res
+  await db("users").insert(user)
+  res = await db("users")
+  expect(res).toHaveLength(1)
+
 })
